84e416450224e1695429ef4707358b1a13879ecf
vivek v <vivekachu15@gmail.com>
2022-09-02 19:48:44 +0530

    Update app-routing.module.ts

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
+
 const routes: Routes = [{path:'',component:LoginComponent},
                         {path:'home',component:HomeComponent}];
 
