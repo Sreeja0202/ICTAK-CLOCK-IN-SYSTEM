dd644a6ddb67bd2b7128f561627c0ca103e67f14
vivek-vvk <vivekachu15@gmail.com>
2022-10-11 21:01:44 +0530

    timer

--- FrontEnd/src/app/timetrackerpage/timetrackerpage.component.ts
+++ FrontEnd/src/app/timetrackerpage/timetrackerpage.component.ts
@@ -14,16 +14,75 @@ export class TimetrackerpageComponent implements OnInit {
   name="Name";
 project=`Project Details`;
 
+
+
+showFirst:boolean=false;
+
+ms:any='0'+0;
+sec:any='0'+0;
+min:any='0'+0;
+hr:any='0'+0;
+
+startTimer:any;
+
+
+
   constructor(private router: Router) {}
 
 
 
   ngOnInit(): void {}
  
+
+  
   logoutUser() {
     localStorage.removeItem('token');
     this.router.navigate(['/login']);
   }
+
+  start():void{
+
+    if(!this.showFirst){
+      this.showFirst=true;
+      this.startTimer=setInterval(()=>{
+        this.ms++;
+        this.ms=this.ms<10 ? '0' + this.ms:this.ms;
+  
+        if(this.ms === 100){
+          this.sec++;
+          this.sec=this.sec< 10 ? '0'+this.sec :this.sec;
+          this.ms='0'+0;
+        }
+        if(this.sec === 60){
+          this.min++;
+          this.min=this.min<10 ? '0' + this.sec :this.sec;
+        }
+        if(this.min===60){
+          this.hr++;
+          this.hr < 10 ? '0' + this.hr : this.hr;
+          this.min='0' + 0;
+        }
+  
+      }, 10); 
+     }else{
+      this.stop();
+     }  
+  }
+  
+  
+  stop():void{
+    clearInterval(this.startTimer);
+    this.showFirst=false;
+  }
+  
+  reset():void{
+    clearInterval(this.startTimer);
+    this.showFirst=false;
+    this.hr=this.min=this.sec=this.ms='0'+ 0;
+  }
+
+
+
 
 
 
