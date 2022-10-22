0872fdfe3abed58e09e9b85e6ff0092e27dbc3ac
nimisha <nimisha.tv001@gmail.com>
2022-10-22 16:42:53 +0530

    hosting

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -7,15 +7,24 @@ const prorouter = require("./routes/project.routes.js");
 const trackerrouter = require("./routes/tracker.routes.js");
 const taskrouter = require("./routes/tasks.routes.js");
 const filterrouter = require("./routes/filters.route.js");
+const path = require('path');
+
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static('./dist/frontend'));
 
 app.use("/employees", emprouter);
 app.use("/projects", prorouter);
 app.use("/trackers", trackerrouter);
 app.use("/tasks", taskrouter);
 app.use("/filters", filterrouter);
+
+app.get('/*',  function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist//frontend/index.html'));
+ });
+ 
 
 app.listen(3000, function () {
   console.log("Server ready in  3000");
